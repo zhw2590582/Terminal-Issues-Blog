5e1c3dbfd11615aba27e81c649c3334170987cba
Harvey <laozhaochaguan@gmail.com>
2020-04-01 10:38:21 +0800

    add more file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-var term = new Terminal();
-term.open(document.getElementById("terminal"));
-term.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+window.Main = (function(Utils, Issues, Comment) {
+  var term = new Terminal();
+  term.loadAddon(new FitAddon.FitAddon());
+  term.loadAddon(new WebLinksAddon.WebLinksAddon());
+  term.open(document.getElementById("terminal"));
+
+  term.write("User $ ");
+
+  term.onKey(function(data) {
+    console.log(data);
+    term.write(data.key);
+    term.writeln("");
+  });
+})(window.Utils, window.Issues, window.Comment);
